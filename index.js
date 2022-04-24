fece754c6cb40f2e66eb3247439f08a913c35e54
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-24 19:23:49 -0400

    installation du middleware npm i -s body-parser

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController'); 
+const bodyParser = require('body-parser');
 
 //*Mise en place du middlware : 
+app.use(bodyParser.json());
 app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500')); 
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,13 +1,20 @@
 const express = require('express');     
 const router = express.Router();       
 
-const { PostsModel} = require('../models/postsModel'); 
+const { PostsModel } = require('../models/postsModel'); 
 
-router.get('/localhost/posts/', (req, res) => {          
+router.get('/', (req, res) => {          
     PostsModel.find((err, docs) => {    
         if (!err) res.send(docs);
         else console.log("Error to get data : " + err); 
     })
-})
+});
+
+router.post('/', (req, res) => {          
+    const newRecord = new PostsModel({
+        author: req.body.author, // req.body ne marche qu avec body parser : 
+        message: req.body.message
+    })
+});
 
 module.exports = router
