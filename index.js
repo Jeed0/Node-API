29605f68f03620d6a174cde0430b71cc9be332bd
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-24 18:21:45 -0400

    mise en place du controller

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express'); // lancer express
 const app = express();
 
-require('./models/dbConfig')
+require('./models/dbConfig');
+
 
 app.listen(5500, () => console.log('Server started: 5500')); // connexion et lancement serveur
 
