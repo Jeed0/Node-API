096d932c063bbbf7d4480b39616b554bc3cedb37
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-24 20:58:04 -0400

    ouvrir aux autres sites avec npm i -s cors

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController'); 
 const bodyParser = require('body-parser');
 // ancienne MAJ pour le put : const mongoose = require('mongoose');
+const cors = require('cors'); //! appel du cors apres npm i -s cors
 
 //*Mise en place du middlware :
 // ancinenne maj pour le put : mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
+app.use(cors()); //! mettre cors avant les routes pour ouvrir l'API aux autre sites sur internet
 app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500')); 
