834362d1e69fa63f3498de86872034afd5786d63
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-24 20:41:03 -0400

    methode put postman ok

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController'); 
 const bodyParser = require('body-parser');
+// ancienne MAJ pour le put : const mongoose = require('mongoose');
 
-//*Mise en place du middlware : 
+//*Mise en place du middlware :
+// ancinenne maj pour le put : mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
 app.use('/posts', postsRoutes);
 
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,5 +1,6 @@
 const express = require('express');     
-const router = express.Router();       
+const router = express.Router();   
+const ObjectID = require('mongoose').Types.ObjectId;    
 
 const { PostsModel } = require('../models/postsModel'); 
 
@@ -21,5 +22,26 @@ router.post('/', (req, res) => {
         else console.log("Error creating new data : " + err);
     })
 })
+
+router.put("/:id", (req, res) => {          //on doit pointer l'id et inclure object ID
+        if (!ObjectID.isValid(req.params.id)) // si id n est pas connu
+            return res.status(400).send("ID unknow : " + req.params.id); // tu me retourne l erreur d'id
+        const updatedRecord = {
+            author : req.body.author,
+            message: req.body.message
+        };
+
+        PostsModel.findByIdAndUpdate(
+            req.params.id, //on recupere l'id de la donnée
+            { $set: updatedRecord },
+            { new:true }, //on met à jour la modification
+            (err, docs) => {
+                if (!err) res.send(docs);
+                else console.log("Update error : " + err);
+            }
+        )
+})
+
+
 
 module.exports = router;
