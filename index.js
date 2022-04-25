a0849aba0423e1d0c7d2d61a74c954c625e41d6a
Jeed0 <83682533+Jeed0@users.noreply.github.com>
2022-04-24 21:01:29 -0400

    cors origin pour preciser le site autorisé

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors'); //! appel du cors apres npm i -s cors
 //*Mise en place du middlware :
 // ancinenne maj pour le put : mongoose.set('useFindAndModify', false);
 app.use(bodyParser.json());
-app.use(cors()); //! mettre cors avant les routes pour ouvrir l'API aux autre sites sur internet
+app.use(cors({origin: 'https://cdpn.io'})); //! mettre cors avant les routes pour ouvrir l'API aux autre sites sur internet
 app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500')); 
